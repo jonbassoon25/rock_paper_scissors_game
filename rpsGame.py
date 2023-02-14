c8f0e178834f77bb1b0455b1ed15aeaa2619b063
jonbassoon25 <jonbassoon25@gmail.com>
2023-02-14 09:30:09 -0700

    added some of rpsls

--- rpsGame.py
+++ rpsGame.py
@@ -1,3 +1,6 @@
+#Secret game mode when clicking red box
+
+
 import random
 import pygame
 
@@ -9,6 +12,10 @@ paper_jpg = pygame.image.load("assets/paper.jpg")
 paper_jpg = pygame.transform.scale(paper_jpg, (200, 200))
 scissors_png = pygame.image.load("assets/scissors.png")
 scissors_png = pygame.transform.scale(scissors_png, (200, 200))
+lizard_png = pygame.image.load("assets/lizard.png")
+lizard_png = pygame.transform.scale(lizard_png, (200, 200))
+spock_png = pygame.image.load("assets/spock.png")
+spock_png = pygame.transform.scale(spock_png, (200, 200))
 
 #vars
 WIDTH = 800
@@ -17,22 +24,11 @@ player1 = ""
 size = 0
 NEEDED_SIZE = 400
 score = [0, 0, 0]
+selection = 0
+#is game mode rock paper scissors lizard spock
+rpsls = False
 
 #functions
-#increases size of rock image, returns new size
-def inflate_rock(rock_png, init_size, amount):
-	rock_png = pygame.transform.scale(rock_png, (init_size + amount, init_size + amount))
-	return [rock_png, init_size + amount]
-
-#increases size of paper image, returns new size
-def inflate_paper(paper_jpg, init_size, amount):
-	paper_jpg = pygame.transform.scale(paper_jpg, (init_size + amount, init_size + amount))
-	return [paper_jpg, init_size + amount]
-
-#increases size of scissors image, returns new size
-def inflate_scissors(scissors_png, init_size, amount):
-	scissors_png = pygame.transform.scale(scissors_png, (init_size + amount, init_size + amount))
-	return [rock_png, init_size + amount]
 
 #draws rock at x, y with size	
 def rock(x, y, size = 200):
@@ -45,6 +41,12 @@ def paper(x, y, size = 200):
 #draws scissors at x, y with size
 def scissors(x, y, size = 200):
 	screen.blit(scissors_png, (x - size/2, y - size/2))
+
+def lizard(x, y, size = 200):
+	screen.blit(lizard_png, (x - size/2, y - size/2))
+
+def spock(x, y, size = 200):
+	screen.blit(spock_png, (x - size/2, y - size/2))
 
 #draws box on screen that changes color when selected
 def box(screen, x, y, selected = False):
@@ -78,6 +80,35 @@ def rps_win(player1, player2):
 			return 'player one'
 		elif player2 == 'scissors':
 			return 'tie'
+	
+def rpsls_win(player1, player2):
+	if player1 == "rock":
+		if player2 == "scissors" or player2 == "lizard":
+			return "player one"
+		elif player2 == "paper" or player2 == "spock":
+			return "player two" 
+	elif player1 == "paper":
+		if player2 == "rock" or player2 == "spock":
+			return "player one"
+		elif player2 == "scissors" or player2 == "lizard":
+			return "player two"
+	elif player1 == "scissors":
+		if player2 == "paper" or player2 == "lizard":
+			return "player one"
+		elif player2 == "spock" or player2 == "rock":
+			return "player two"
+	elif player1 == "lizard":
+		if player2 == "spock" or player2 == "paper":
+			return "player one"
+		elif player2 == "scissors" or player2 == "rock":
+			return "player two"
+	elif player1 == "spock":
+		if player2 == "scissors" or player2 == "rock":
+			return "player one"
+		if player2 == "paper" or player2 == "lizard":
+			return "player two"
+	else:
+		return
 
 #returns computer selection for rock paper scissors
 def computer():
@@ -89,8 +120,7 @@ def computer():
 
 #displays rock paper and scissors images with outlines
 def rps_lineup(selection):
-	if selection == 0:
-		select = [False, False, False]
+	select = [False, False, False]
 	if selection == 1:
 		select = [True, False, False]
 	if selection == 2:
@@ -107,6 +137,40 @@ def rps_lineup(selection):
 	box(screen, 650, 250, select[2])
 	scissors(650, 250)
 	display_text("Scissors", 550, 355)
+
+def rpsls_lineup(selection):
+	select = [False, False, False, False, False]
+	if selection == 1:
+		select = [True, False, False, False, False]
+	if selection == 2:
+		select = [False, True, False, False, False]
+	if selection == 3:
+		select = [False, False, True, False, False]
+	if selection == 4:
+		select = [False, False, False, True, False]
+	if selection == 5:
+		select = [False, False, False, False, True]
+	
+	box(screen, 150, 150, select[0])
+	rock(150, 150)
+	display_text("Rock", 100, 255)
+
+	box(screen, 400, 150, select[1])
+	paper(400, 150)
+	display_text("Paper", 335, 255)
+
+	box(screen, 650, 150, select[2])
+	scissors(650, 150)
+	display_text("Scissors", 550, 255)
+
+	box(screen, 650, 450, select[3])
+	lizard(650, 450)
+	display_text("Lizard", 600, 555)
+
+	box(screen, 150, 450, select[4])
+	spock(150, 450)
+	display_text("Spock", 100, 555)
+	
 
 def display_text(text, x = 100, y = 400, size = 25):
 	font = pygame.font.SysFont("monospace", size)
@@ -160,6 +224,8 @@ while active:
 			if mouse_x >= 725 and mouse_x <= 800 and mouse_y >= 0 and mouse_y <= 25:
 				player1 = ''
 				player2 = ''
+			if mouse_x >= 750 and mouse_x <= 800 and mouse_y >= 550 and mouse_y <= 600:
+				rpsls = True
 			
 	#calculate events
 	#Gets mouse position
@@ -173,7 +239,21 @@ while active:
 		selection = 3
 	else:
 		selection = 0
-	
+
+	if player1 != '' and not score_added:
+		winner = rps_win(player1, player2)
+	else:
+		winner = ''
+
+	if winner == 'player one':
+		score[0] += 1
+		score_added = True
+	elif winner == 'computer':
+		score[1] += 1
+		score_added = True
+	elif winner == 'tie':
+		score[2] += 1
+		score_added = True
 
 	#update screen
 	screen.fill("#cccccc")
@@ -203,22 +283,16 @@ while active:
 		display_text("Computer", 545, 100)
 		display_text("Winner", 350, 400)
 		display_text(rps_win(player1, player2), 325, 450)
-
-		if not score_added:
-			winner = rps_win(player1, player2)
-		else:
-			winner = ''
-		if winner == 'player one':
-			score[0] += 1
-			score_added = True
-		elif winner == 'computer':
-			score[1] += 1
-			score_added = True
-		elif winner == 'tie':
-			score[2] += 1
-			score_added = True
 		
 		back_button(screen, 725, 0)
+	else:
+		if not rpsls:
+			rps_lineup(selection)
+		else:
+			rpsls_lineup(selection)
+		score_added = False
+	
+	pygame.draw.rect(screen, "red", (750, 550, 50, 50))
 		
 		
 		
@@ -226,9 +300,7 @@ while active:
 		#display_text(f"Computer Selected: {player2.title()}", 100, 450)
 		#display_text(f"The Winner is: {rps_win(player1, player2).title()}", 100, 500)
 
-	else:
-		rps_lineup(selection)
-		score_added = False
+	
 	
 	display_text(f"Wins: {score[0]}    Losses: {score[1]}    Ties: {score[2]}", 25, 25)
 	
