ea56b8ee033b2a7183495a8fefd0aa910d30655e
jonbassoon25 <jonbassoon25@gmail.com>
2023-02-14 09:04:34 -0700

    added new features and pictures

--- rpsGame.py
+++ rpsGame.py
@@ -16,6 +16,7 @@ HEIGHT = 600
 player1 = ""
 size = 0
 NEEDED_SIZE = 400
+score = [0, 0, 0]
 
 #functions
 #increases size of rock image, returns new size
@@ -58,21 +59,21 @@ def box(screen, x, y, selected = False):
 def rps_win(player1, player2):
 	if player1 == 'rock':
 		if player2 == 'paper':
-			return 'player two'
+			return 'computer'
 		elif player2 == 'scissors':
 			return 'player one'
 		elif player2 == 'rock':
 			return 'tie'
 	elif player1 == 'paper':
 		if player2 == 'scissors':
-			return 'player two'
+			return 'computer'
 		elif player2 == 'rock':
 			return 'player one'
 		elif player2 == 'paper':
 			return 'tie'
 	elif player1 == 'scissors':
 		if player2 == 'rock':
-			return 'player two'
+			return 'computer'
 		elif player2 == 'paper':
 			return 'player one'
 		elif player2 == 'scissors':
@@ -112,6 +113,9 @@ def display_text(text, x = 100, y = 400, size = 25):
 	sentence = font.render(text, True, "black")
 	screen.blit(sentence, (x, y))
 
+def back_button(screen, x, y):
+	pygame.draw.rect(screen, "black", (x, y, 75, 25), 2)
+	display_text("Back", x + 7, y)
 	
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 active = True
@@ -138,21 +142,24 @@ while active:
 				player2 = computer()
 				selection = 3
 
-		if event.type == pygame.MOUSEBUTTONDOWN and player1 == '':
+		if event.type == pygame.MOUSEBUTTONDOWN:
 			#gets position of mouse click and assignes to mouse_x and mouse_y
 			mouse_x, mouse_y = event.pos
 			#Check if something is pressed
-			if mouse_x >= 50 and mouse_x <= 250 and mouse_y >= 150 and mouse_y <= 350:
+			if mouse_x >= 50 and mouse_x <= 250 and mouse_y >= 150 and mouse_y <= 350 and player1 == '':
 				player1 = "rock"
 				player2 = computer()
 
-			if mouse_x >= 300 and mouse_x <= 500 and mouse_y >= 150 and mouse_y <= 350:
+			if mouse_x >= 300 and mouse_x <= 500 and mouse_y >= 150 and mouse_y <= 350 and player1 == '':
 				player1 = "paper"
 				player2 = computer()
 
-			if mouse_x >= 550 and mouse_x <= 750 and mouse_y >= 150 and mouse_y <= 350:
+			if mouse_x >= 550 and mouse_x <= 750 and mouse_y >= 150 and mouse_y <= 350 and player1 == '':
 				player1 = "scissors"
 				player2 = computer()
+			if mouse_x >= 725 and mouse_x <= 800 and mouse_y >= 0 and mouse_y <= 25:
+				player1 = ''
+				player2 = ''
 			
 	#calculate events
 	#Gets mouse position
@@ -172,23 +179,47 @@ while active:
 	screen.fill("#cccccc")
 	if player1 != "":
 		if player1 == 'rock':
-			rock(150, 250)
+			rock(200, 250)
 		
 		if player1 == 'paper':
-			paper(150, 250)
+			paper(200, 250)
 
 		if player1 == 'scissors':
-			scissors(150, 250)
+			scissors(200, 250)
 
 		if player2 == 'rock':
-			rock(550, 250)
+			rock(600, 250)
 		
 		if player2 == 'paper':
-			paper(550, 250)
+			paper(600, 250)
 
 		if player2 == 'scissors':
-			scissors(550, 250)
+			scissors(600, 250)
 
+		box(screen, 200, 250)
+		box(screen, 600, 250)
+		
+		display_text("Player", 145, 100)
+		display_text("Computer", 545, 100)
+		display_text("Winner", 350, 400)
+		display_text(rps_win(player1, player2), 325, 450)
+
+		if not score_added:
+			winner = rps_win(player1, player2)
+		else:
+			winner = ''
+		if winner == 'player one':
+			score[0] += 1
+			score_added = True
+		elif winner == 'computer':
+			score[1] += 1
+			score_added = True
+		elif winner == 'tie':
+			score[2] += 1
+			score_added = True
+		
+		back_button(screen, 725, 0)
+		
 		
 		
 		#display_text(f"You Selected: {player1.title()}")
@@ -197,7 +228,9 @@ while active:
 
 	else:
 		rps_lineup(selection)
-		
+		score_added = False
+	
+	display_text(f"Wins: {score[0]}    Losses: {score[1]}    Ties: {score[2]}", 25, 25)
 	
 	pygame.display.flip()
 
